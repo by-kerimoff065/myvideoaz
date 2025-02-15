43e77b65021416ee31ac61552d693f6595ac76c2
by-kerimoff <tv65az@gmail.com>
2025-02-16 00:55:39 +0400

    Update main.py

--- main.py
+++ main.py
@@ -37,4 +37,20 @@ def send_email(config):
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
-        server
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+        server.quit()
+        print("E-poçt uğurla göndərildi!")
+    except Exception as e:
+        print(f"E-poçt göndərilmədi: {e}")
+
+# Yeni link yaratmaq
+def generate_stream_url(config):
+    url = f"https://mydo.canlitvplayer.com/xazer.m3u8?bandwidth=2096&e={config['e']}&playlistlength=5&shift=0&sid=coder_53&token={config['token']}&user=3707"
+    return url
+
+# Skripti işə salmaq
+if __name__ == "__main__":
+    config = read_config()
+    update_token(config)
+    send_email(config)
